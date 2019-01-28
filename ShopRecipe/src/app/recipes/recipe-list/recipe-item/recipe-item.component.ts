607d1546b0ee0d52fd3063b4ca15a1074415a355
Benedictiknkeonye@gmail.com <Benedictiknkeonye@gmail.com>
2019-01-28 10:36:56 +0100

    Component DataBinding

--- ShopRecipe/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ ShopRecipe/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,5 @@
 import { Recipe } from './../../recipe.model';
-import { Component, OnInit, Input, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
@@ -8,7 +8,7 @@ import { Component, OnInit, Input, EventEmitter } from '@angular/core';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
-  recipeSelected = new EventEmitter<void>();
+  @Output() recipeSelected = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit() {
--- ShopRecipe/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ShopRecipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,15 +11,15 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'Simply a test',
+      'Sunday Special',
+      'White Rice',
       'https://upload.wikimedia.org/wikipedia/commons/9/9b/NIgerian_food.jpg'
     ),
 
     new Recipe(
-      'A Test Recipe',
-      'Simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/9/9b/NIgerian_food.jpg'
+      'Your Breakfast',
+      'Fried Rice & Turkey',
+      'https://i1.wp.com/www.nigerianlazychef.com/wp-content/uploads/2016/12/IMG_6781.jpg'
     )
   ];
   constructor() { }
